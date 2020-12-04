36481a2eaed1b361851377b5a9426afae601b998
LUO12826 <luohuizhou@hotmail.com>
2020-12-04 14:40:11 +0800

    CMMM语言测试

---
+++ IDE-UI/CMMM/outputStreamDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public interface outputStreamDelegate
+{
+    void Print(String s);
+}
+
+public class outputStream
+{
+    public virtual void Print(string msg)
+    {
+        System.Console.WriteLine(msg);
+    }
+}
+
--- IDE-UI/MainWindow.xaml.cs
+++ IDE-UI/MainWindow.xaml.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace IDE_UI
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, outputStreamDelegate
     {
         public MainWindow()
         {
             InitializeComponent();
+            textBox.FontSize = 12;
+        }
+
+
+        private void loadSample_Click(object sender, RoutedEventArgs e)
+        {
+            textEditor.Text = "write(\"CMMM语言while循环示例:\");\nnumber a = 10;\nwhile (a <> 0) {\n\ta = a - 1;\n\twrite(a + \" \");\n}";
+        }
+
+        private void run_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            String input = textEditor.Text;
+            ICharStream stream = CharStreams.fromstring(input);
+            ITokenSource lexer = new CMMMLexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            CMMMParser parser = new CMMMParser(tokens);
+            parser.BuildParseTree = true;
+            IParseTree tree = parser.program();
+            var visitor = new TestVisitor();
+            visitor.outputStream = this;
+            visitor.Visit(tree);
+        }
+
+        public void Print(string s)
+        {
+            textBox.Text += s;
         }
     }
 }
