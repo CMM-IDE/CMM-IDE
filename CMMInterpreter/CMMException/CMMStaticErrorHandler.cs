e750c0ed06c0ad4f58e45ec7398fd568586a9eb6
crdv7 <15974123760@163.com>
2020-12-17 23:57:11 +0800

    StaticErrorHandler修改

--- CMMInterpreter/CMMException/CMMStaticErrorHandler.cs
+++ CMMInterpreter/CMMException/CMMStaticErrorHandler.cs
@@ -8,6 +8,11 @@ namespace CMMInterpreter.CMMException
 {
    public class CMMStaticErrorHandler
     {
+        /*
+         * 调用实例：
+         * CMMStaticErrorHandler handler=new CMMStaticErrorHandler(IOutputstream对象);
+         * handler.StaticError(输入字符);
+         */
         public IOutputStream outputStream;
         public CMMStaticErrorHandler(IOutputStream outputStream)
         {
@@ -16,27 +21,25 @@ namespace CMMInterpreter.CMMException
         public void StaticError(String input)
         {
             try{
-                ICharStream stream = CharStreams.fromstring(input);
+            ICharStream stream = CharStreams.fromstring(input);
             ITokenSource lexer = new CMMLexer(stream);
             ITokenStream tokens = new CommonTokenStream(lexer);
             CMMParser parser = new CMMParser(tokens);
             parser.BuildParseTree = true;
-            parser.RemoveErrorListeners();
+            parser.RemoveErrorListeners();//移除默认监听器
             CMMErrorListener errorListener = new CMMErrorListener();
             errorListener.outputStream = this.outputStream;
-            parser.AddErrorListener(errorListener);
+            parser.AddErrorListener(errorListener);//添加自定义错误监听器
             CMMErrorStrategy errorStrategy = new CMMErrorStrategy();
-            parser.ErrorHandler = errorStrategy;
+            parser.ErrorHandler = errorStrategy;//添加自定义错误策略
                 parser.statements();
                 }
                 catch (RuntimeException e1)
                 {
                     outputStream?.Print("Line:" + e1.line.ToString() + " " + e1.Message);
-                    //Print(e1.Message);
                 }
-                catch (Exception e2)
+                catch (Exception)
                 {
-                //outputStream?.Print(e2.Message);
                 }
             
         }
